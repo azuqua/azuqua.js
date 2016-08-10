ef6698932827786f841b28216fafe8152b178261
Holden Stegman <holden@azuqua.com>
2016-08-10 10:19:38 -0700

    Doc updates, removed old docs. Minor test changes

--- src/azuqua.js
+++ src/azuqua.js
@@ -1,8 +1,13 @@
 'use strict';
 
+// Standard Lib modules
 const url = require('url');
 const crypto = require('crypto');
 const querystring = require('querystring');
+const path = require('path');
+const fs = require('fs');
+
+// External modules
 const fetch = require('node-fetch');
 const Promise = require('bluebird');
 const _ = require('lodash');
@@ -10,6 +15,13 @@ const _ = require('lodash');
 fetch.Promise = Promise;
 
 const paramRegex = /:\w+/;
+
+/**
+ * `AzuquaCallback`
+ * @callback azuquaCallback
+ * @param {object} Error - An error object containing information about the error
+ * @param {object} Response - A response object containing relevant response information
+ */
 
 function checkResponseError(response) {
   if (response.status < 200 || response.status >= 400) {
@@ -26,13 +38,39 @@ function toJSON(response) {
   return response.json();
 }
 
+function errorHandler(error) {
+  if (error.name === 'FetchError') {
+    let formattedError = new Error('Error reaching requested resource');
+    formattedError.name = 'Request Error';
+    throw formattedError;
+  } else {
+    throw error;
+  }
+}
+
+function handleErrorCallback(cb) {
+  return function(error) {
+    cb(error, null);
+  };
+}
+
+/** Class representing an Azuqua instance */
 class Azuqua {
-  // Constructor
+  /**
+   * Creates an azuqua instance with given Access Key and Access Secret
+   * @constructor
+   * @param {string} accessKey - The access key associated with the Azuqua account
+   * @param {string} accessSecret - The access secret associated with the Azuqua account
+   * @param {object} [httpOptions] - An httpOptions object to override default httpOptions
+   */
   constructor(accessKey, accessSecret, httpOptions) {
       // Properties
     this.account = {};
-    this.account.accessKey = null;
-    this.account.accessSecret = null;
+    this.account.accessKey = undefined;
+    this.account.accessSecret = undefined;
+
+    // Cached Flos
+    this._flos = null;
 
     if(process.env.ACCESS_KEY) {
       this.account.accessKey = process.env.ACCESS_KEY;
@@ -65,30 +103,111 @@ class Azuqua {
     Object.freeze(this.httpOptions);
   } // End of constructor
 
-  // (Get) Flos method
-  flos(cb) {
+  /**
+   * Read all flos related to the given user account
+   * @example
+   * // Returns a list of flo objects - forcing the method to ignore caching
+   * azuqua.flos(true).then(function(flo) {
+   *  console.log(data)
+   * }).catch(function(error) {
+   *  console.log('Error: ', error);
+   * })
+   * @param {boolean} [force=false] - Option to force flos to ignore cached flos and make new network request
+   * @param {azuquaCallback} [cb] - Callback function that handles flos response
+   */
+  flos(force, cb) {
+    if (typeof force === 'function') {
+      cb = force;
+      force = false;
+    } else if (typeof force !== 'function' && typeof cb !== 'function') {
+      return Promise.promisify(this.flos).bind(this)(force);
+    }
+    if (!force && this._flos !== null) {
+      return cb(null, this._flos);
+    } 
     this.makeRequest('GET', Azuqua.routes.flos.path, {})
       .then((json) => {
-        cb(null, json);
+        this._flos = json.map((flo) => new Flo(flo));
+        cb(null, this._flos);
       }).catch((error) => {
         cb(error, null);
       });
   } // End get flos method
 
-  // Invoke flos method
+  /**
+   * Read information about a particular flo
+   * @param {Flo|string} flo - The flo object or alias of a flo
+   * @param {azuquaCallback} [cb] - Callback function that handles read response
+   */
+  read(flo, cb) {
+    if (typeof cb !== 'function') {
+      return Promise.promisify(this.read).bind(this)(flo);
+    }
+    let alias = typeof flo === 'string' ? flo : flo.alias;
+    let params = { alias : alias };
+    this.makeRequest('GET', '/flo/:alias/read', params)
+      .then((json) => {
+        cb(null, json);
+      }).catch(handleErrorCallback(cb));
+  }
+
+  /**
+   * Invoke a flo with the given parameter information
+   * @param {Flo|string} flo - The flo object or alias of a flo
+   * @param {object} params - An object containing the params needed to invoke the flo
+   * @param {azuquaCallback} [cb] - Callback function that handles the invoke response
+   */
   invoke(flo, params, cb) {
+    if (typeof cb !== 'function') {
+      return Promise.promisify(this.invoke).bind(this)(flo, params);
+    }
     let alias = typeof flo === 'string' ? flo : flo.alias;
     params.alias = alias;
     this.makeRequest('POST', Azuqua.routes.invoke.path, params)
       .then((json) => {
         cb(null, json);
-      }).catch((error) => {
-        cb(error, null);
-      });
+      }).catch(handleErrorCallback(cb));
   }
 
-  // Make request method
-  makeRequest(_method, _path, _params) {
+  /**
+   * Read the inputs of a particular flo
+   * @param {Flo|string} flo - The flo object or alias of a flo
+   * @param {azuquaCallback} [cb] - Callback function that handles the inputs response
+   */
+  inputs(flo, cb) {
+    if (typeof cb !== 'function') {
+      return Promise.promisify(this.inputs).bind(this)(flo);
+    }
+    let alias = typeof flo === 'string' ? flo : flo.alias;
+    let params = {};
+    params.alias = alias;
+    this.makeRequest('GET', Azuqua.routes.inputs.path, params)
+      .then((json) => {
+        cb(null, json);
+      }).catch(handleErrorCallback(cb));
+  }
+
+  /**
+   * Returns the orgs related to the particular user
+   * @param {azuquaCallback} [cb] - Callback function that handles the inputs response
+   */
+  groups(cb) {
+    if (typeof cb !== 'function') {
+      return Promise.promisify(this.groups).bind(this)();
+    }
+    this.makeRequest('GET', 'user/orgs', {})
+      .then((json) => {
+        cb(null, json);
+      }).catch(handleErrorCallback(cb));
+  }
+
+  /**
+   * A generic function that allows request to arbitrary routes. Handles building the headers before sending the request
+   * @param {string} _method - The HTTP method
+   * @param {string} _path - The path of the desired resource. 
+   * @param {object} [_params] - Additional params that will be parsed and passed to the request
+   */
+  makeRequest(_method, _path, _params = {}) {
     if (!this.account.accessKey || !this.account.accessSecret) {
       return Promise.reject(new Error('Account information not provided'));
     }
@@ -131,6 +250,7 @@ class Azuqua {
     }
     route = formattedUrl + (Object.keys(_.omit(_params, ['data'])).length > 0 ? 
             '?' + querystring.stringify(_.omit(_params, ['data'])) : '');
+    console.log('QUERY: ', route);
 
     let authHeaders = Azuqua.generateHeaders(method, route, this.account.accessKey, 
         this.account.accessSecret, _params);
@@ -147,6 +267,7 @@ class Azuqua {
     // });
 
     // The .then basically triggers the error handler on our flo methods
+    console.log(requestUrl);
     return fetch(requestUrl, {
       method: method,
       headers : {
@@ -154,15 +275,35 @@ class Azuqua {
         ...authHeaders
       },
       body : data
-    }).then(checkResponseError).then(toJSON);
+    }).then(checkResponseError).then(toJSON).catch(errorHandler);
   } // End make request method
 
+  loadConfig (_path) {
+    // Resolve the config path from the requiring parent's location if it's relative
+    _path = path.resolve(path.dirname(module.parent.filename), _path);
+    this.account = require(_path);
+  };
 
-
+  loadConfigAsync (_path, cb) {
+    _path = path.resolve(path.dirname(module.parent.filename), _path);
+    fs.readFile(_path, (error, data) => {
+      if (error) {
+        cb(error, null);
+      } else {
+        try {
+          let config = JSON.parse(data);
+          this.account = config;
+          cb(null, config);
+        } catch (e) {
+          cb(e, null);
+        }
+      }
+    });
+  }
 
   // Start generateHeaders method
   static generateHeaders(method, path, accessKey, accessSecret, params = '') {
-        // Default params don't override null and we can't send null data
+    // Default params don't override null and we can't send null data
     let data = params ? params.data : '';
     if (!data) {
       data = '';
@@ -177,7 +318,9 @@ class Azuqua {
       let queryObject = url.parse(path, true).query;
       if (Object.keys(queryObject).length > 0) {
         pathQueryString = JSON.stringify(queryObject);
-      };
+      } else {
+        pathQueryString = '';
+      }
     } else {
       pathQueryString = data || '{}';
     }
@@ -185,14 +328,20 @@ class Azuqua {
     let timestamp = new Date().toISOString();
 
     let meta = [method.toLowerCase(), path, new Date().toISOString()].join(':') + pathQueryString;
+    console.log(meta);
     let hash = crypto.createHmac('sha256', accessSecret).update(new Buffer(meta, 'utf-8')).digest('hex');
 
-
     return {
-      'x-api-hash' : hash,
-      'x-api-accesskey' : accessKey,
-      'x-api-timestamp' : timestamp
+      'x-publisher-hash' : hash,
+      'x-publisher-key' : accessKey,
+      'x-publisher-timestamp' : timestamp
     };
+
+    // return {
+    //   'x-api-hash' : hash,
+    //   'x-api-accesskey' : accessKey,
+    //   'x-api-timestamp' : timestamp
+    // };
   } // End of generateHeaders
 
   // Declare routes
@@ -229,8 +378,7 @@ class Azuqua {
 
 class Flo {
   constructor(floObject) {
-    this.flo = floObject;
-    this.alias = floObject.alias;
+    _.extend(this, floObject);
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -1,291 +1,259 @@
 
-var assert = require("chai").assert,
-	Azuqua = require("../azuqua");
+var assert = require('chai').assert;
+var Azuqua = require('../azuqua.js/azuqua.js');
 
 // not real
 var testCredentials = {
-    accessKey: "d9da0ea5efb58b22545f909e7754235bb9e7fad5",
-    accessSecret: "5686f7797cd31e366608b08fb9460a9926facacd876bb5f70cf872083a34f2cb"
+  accessKey: 'OxsgbNsK7ZDcrrAxKKo3qqd4uKmZwRH5CMAShXw8',
+  accessSecret: 'qqd4uKmZwRH5CMAShXw8L5K0NWf2WYU1dNo8H5ClPPTcatdBLRGk5TFrYw56'
 };
 
 // if you're looking at this file for examples on how to instantiate the azuqua client
 // please do not do it this way. it's better to store your credentials in a .json file
 // and to load them with azuqua.loadConfig() or azuqua.loadConfigAsync()
 
-describe("Azuqua client instantiation tests", function(){
-	this.timeout(40000);
+describe('Azuqua client instantiation tests', function () {
+  this.timeout(40000);
 
-	describe("Client instantiation without credentials", function(){
-		var azuqua = new Azuqua();
+  describe('Client instantiation without credentials', function () {
+		  var azuqua = new Azuqua();
 
-		it("Client should not start with any account information", function(){
-			assert.typeOf(azuqua.account.accessKey, "undefined");
-			assert.typeOf(azuqua.account.accessSecret, "undefined");
-		});
+		  it('Client should not start with any account information', function () {
+      assert.typeOf(azuqua.account.accessKey, 'undefined');
+      assert.typeOf(azuqua.account.accessSecret, 'undefined');
+    });
 
-	});
+  });
 
-	describe("Client instantiation with credentials", function(){
-		var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
+  describe('Client instantiation with credentials', function () {
+		  var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
 
-		it("Client should have an account property of type object", function(){
-			assert.typeOf(azuqua.account, "object");
-		});
+		  it('Client should have an account property of type object', function () {
+      assert.typeOf(azuqua.account, 'object');
+    });
 
-		it("Client should have a non-null accessKey of type string", function(){
-			assert.typeOf(azuqua.account.accessKey, "string");
-			assert.lengthOf(azuqua.account.accessKey, 40);
-		});
+		  it('Client should have a non-null accessKey of type string', function () {
+      assert.typeOf(azuqua.account.accessKey, 'string');
+      assert.lengthOf(azuqua.account.accessKey, 40);
+    });
 
-		it("Client should have a non-null accessSecret of type string", function(){
-			assert.typeOf(azuqua.account.accessSecret, "string");
-			assert.lengthOf(azuqua.account.accessSecret, 64);
-		});
+		  it('Client should have a non-null accessSecret of type string', function () {
+      assert.typeOf(azuqua.account.accessSecret, 'string');
+      assert.lengthOf(azuqua.account.accessSecret, 64);
+    });
 
-	});
+  });
 
-	describe("Client instantiation with environment variables", function(){
-		process.env.ACCESS_KEY = testCredentials.accessKey;
-		process.env.ACCESS_SECRET = testCredentials.accessSecret;
-		var azuqua = new Azuqua();
+  describe('Client instantiation with environment variables', function () {
+		  process.env.ACCESS_KEY = testCredentials.accessKey;
+		  process.env.ACCESS_SECRET = testCredentials.accessSecret;
+		  var azuqua = new Azuqua();
 
-		it("Client should have an account property of type object", function(){
-			assert.typeOf(azuqua.account, "object");
-		});
+		  it('Client should have an account property of type object', function () {
+      assert.typeOf(azuqua.account, 'object');
+    });
 
-		it("Client should have a non-null accessKey of type string", function(){
-			assert.typeOf(azuqua.account.accessKey, "string");
-			assert.lengthOf(azuqua.account.accessKey, 40);
-		});
+		  it('Client should have a non-null accessKey of type string', function () {
+      assert.typeOf(azuqua.account.accessKey, 'string');
+      assert.lengthOf(azuqua.account.accessKey, 40);
+    });
 
-		it("Client should have a non-null accessSecret of type string", function(){
-			assert.typeOf(azuqua.account.accessSecret, "string");
-			assert.lengthOf(azuqua.account.accessSecret, 64);
-		});
+		  it('Client should have a non-null accessSecret of type string', function () {
+      assert.typeOf(azuqua.account.accessSecret, 'string');
+      assert.lengthOf(azuqua.account.accessSecret, 64);
+    });
 
-		process.env.ACCESS_KEY = false;
-		process.env.ACCESS_SECRET = false;
+		  process.env.ACCESS_KEY = false;
+		  process.env.ACCESS_SECRET = false;
 
-	});
+  });
 
 
 });
 
-describe("Client configuration tests", function(){
-	this.timeout(60000);
+describe('Client configuration tests', function () {
+  this.timeout(60000);
 
-	describe("Client configuration with loadConfig", function(){
+  describe('Client configuration with loadConfig', function () {
 
-		var azuqua = new Azuqua();
-		azuqua.loadConfig("./account.json");
+		  var azuqua = new Azuqua();
+		  azuqua.loadConfig('./account.json');
 
-		it("Client should have a non-null accessKey of type string", function(){
-			assert.typeOf(azuqua.account.accessKey, "string");
-			assert.lengthOf(azuqua.account.accessKey, 40);
-		});
+		  it('Client should have a non-null accessKey of type string', function () {
+      assert.typeOf(azuqua.account.accessKey, 'string');
+      assert.lengthOf(azuqua.account.accessKey, 40);
+    });
 
-		it("Client should have a non-null accessSecret of type string", function(){
-			assert.typeOf(azuqua.account.accessSecret, "string");
-			assert.lengthOf(azuqua.account.accessSecret, 64);
-		});
+		  it('Client should have a non-null accessSecret of type string', function () {
+      assert.typeOf(azuqua.account.accessSecret, 'string');
+      assert.lengthOf(azuqua.account.accessSecret, 64);
+    });
 
-	});
+  });
 
-	describe("Client configuration with loadConfigAsync", function(){
-		var azuqua = new Azuqua();
+  describe('Client configuration with loadConfigAsync', function () {
+		  var azuqua = new Azuqua();
 
-		it("Client should asynchronously load configuration data", function(done){
+		  it('Client should asynchronously load configuration data', function (done) {
 
-			azuqua.loadConfigAsync("./account.json", function(error, resp){
-				assert.typeOf(azuqua.account.accessKey, "string");
-				assert.lengthOf(azuqua.account.accessKey, 40);
-				assert.typeOf(azuqua.account.accessSecret, "string");
-				assert.lengthOf(azuqua.account.accessSecret, 64);
-				done();
-			});
+      azuqua.loadConfigAsync('./account.json', function (error, resp) {
+        assert.typeOf(azuqua.account.accessKey, 'string');
+        assert.lengthOf(azuqua.account.accessKey, 40);
+        assert.typeOf(azuqua.account.accessSecret, 'string');
+        assert.lengthOf(azuqua.account.accessSecret, 64);
+        done();
+      });
 
-		});
+    });
 
-	});
+  });
 
 });
 
-describe("Signing function", function() {
-	describe("With latin charset only", function(){
-		var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
-		
-		var hash = azuqua.signData("Hello", "POST", "path", "timestamp");
-			
-		it("Hash should match", function(){
-			assert.equal(hash, "59423b18b5800daaf3ef9eb68b9a35501acfedd5ed0b0de7d4fd9b4c360fed3d");
-		});
-	});
-	
-	describe("With UTF-8 characters", function(){
-		var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
-		
-		var hash = azuqua.signData("你好", "POST", "path", "timestamp");
-		
-		it("Hash should match", function(){
-			assert.equal(hash, "2424f6a6ac168badb8096aae779a6c91287f2e20c19a49598c6c679796b0753a");
-		});
-	});
-	
-	describe("With invalid configuration", function(){
-		var azuqua = new Azuqua(testCredentials.accessKey, '');
-		
-		var fn = function() {
-			return azuqua.signData("Hello", "POST", "path", "timestamp");
-		};
-		
-		it("Should throw Error", function(){
-			assert.throw(fn, Error);
-		});
-	});
-});
+// TODO - ADD SIGNING FUNCITON TESTS
 
-describe("Client API function tests", function(){
-	this.timeout(60000);
+describe('Client API function tests', function () {
+  this.timeout(60000);
 
-	describe("Client API functions with callbacks", function(){
+  describe('Client API functions with callbacks', function () {
 
-		describe("Client flo metadata tests", function(){
+		  describe('Client flo metadata tests', function () {
 
-			describe("Client floMap cache test", function(){
-				
-				var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
+      describe('Client floMap cache test', function () {
 
-				it("Client should not start with any cached flos", function(){
-					assert.typeOf(azuqua.floMap, "undefined");
-				});
+        var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
 
-			});
+        it('Client should not start with any cached flos', function () {
+          assert.typeOf(azuqua.floMap, 'undefined');
+        });
 
-			describe("Client floMap refresh tests", function(){
-				var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
+      });
 
-				it("Client should list published flos as an array of strings", function(done){
-					azuqua.flos(function(error, flos){
-						assert.isNull(error);
-						assert.instanceOf(flos, Array);
-						done();
-					});
-				});
+      describe('Client floMap refresh tests', function () {
+        var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
 
-			});
+        it('Client should list published flos as an array of strings', function (done) {
+          azuqua.flos(function (error, flos) {
+            assert.isNull(error);
+            assert.instanceOf(flos, Array);
+            done();
+          });
+        });
 
-		});
+      });
 
-		describe("Client flo invocation tests", function(){
+    });
 
-			var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
-			
-			before(function(done){
-				azuqua.flos(function(error, flos){
-					done();
-				});
-			});
+		  describe('Client flo invocation tests', function () {
 
-			it("Test flo should return the same starting data", function(done){
-				azuqua.invoke("httptohttp", { something: 1 }, function(error, data){
-					assert.isNull(error);
-					assert.ok(data);
-					done();
-				});
-			});
+      var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
 
-			it("Should allow users to invoke flos with the alias", function(done){
-				azuqua.invoke("3f8ca2b96024cae4cdacf652b6a322", { something: 1 }, function(error, data){
-					assert.isNull(error);
-					assert.ok(data);
-					done();
-				});
-			});
+      before(function (done) {
+        azuqua.flos(function (error, flos) {
+          done();
+        });
+      });
 
-			it("Should allow users to force a flo invoke without using the cache", function(done){
-				azuqua.invoke("3f8ca2b96024cae4cdacf652b6a322", { something: 1 }, true, function(error, data){
-					assert.isNull(error);
-					assert.ok(data);
-					done();
-				});
-			});
+      it('Test flo should return the same starting data', function (done) {
+        azuqua.invoke('httptohttp', { something: 1 }, function (error, data) {
+          assert.isNull(error);
+          assert.ok(data);
+          done();
+        });
+      });
 
-			it("Test flo should fail if the accessSecret is invalid", function(done){
-				azuqua.account.accessSecret = "1";
-				azuqua.invoke("httptohttp", { something: 1 }, function(error, data){
-					assert.typeOf(data, "undefined");
-					assert.isNotNull(error);
-					done();
-				});
-			});
+      it('Should allow users to invoke flos with the alias', function (done) {
+        azuqua.invoke('3f8ca2b96024cae4cdacf652b6a322', { something: 1 }, function (error, data) {
+          assert.isNull(error);
+          assert.ok(data);
+          done();
+        });
+      });
 
-		});
+      it('Should allow users to force a flo invoke without using the cache', function (done) {
+        azuqua.invoke('3f8ca2b96024cae4cdacf652b6a322', { something: 1 }, true, function (error, data) {
+          assert.isNull(error);
+          assert.ok(data);
+          done();
+        });
+      });
+
+      it('Test flo should fail if the accessSecret is invalid', function (done) {
+        azuqua.account.accessSecret = '1';
+        azuqua.invoke('httptohttp', { something: 1 }, function (error, data) {
+          assert.typeOf(data, 'undefined');
+          assert.isNotNull(error);
+          done();
+        });
+      });
+
+    });
 
 
-	});
+  });
 
-	describe("Client API functions with promises", function(){
+  describe('Client API functions with promises', function () {
 
-		describe("Client flo metadata tests", function(){
+		  describe('Client flo metadata tests', function () {
 
-			describe("Client floMap cache test", function(){
-				
-				var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
+      describe('Client floMap cache test', function () {
 
-				it("Client should not start with any cached flos", function(){
-					assert.typeOf(azuqua.floMap, "undefined");
-				});
+        var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
 
-			});
+        it('Client should not start with any cached flos', function () {
+          assert.typeOf(azuqua.floMap, 'undefined');
+        });
 
-			describe("Client floMap refresh tests", function(){
-				var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
+      });
 
-				it("Client should list published flos as strings", function(done){
-					azuqua.flos().then(function(flos){
-						assert.instanceOf(flos, Array);
-						done();
-					});
-				});
+      describe('Client floMap refresh tests', function () {
+        var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
 
-			});
+        it('Client should list published flos as strings', function (done) {
+          azuqua.flos().then(function (flos) {
+            assert.instanceOf(flos, Array);
+            done();
+          });
+        });
 
-		});
+      });
 
-		describe("Client flo invocation tests", function(){
+    });
 
-			var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
-			
-			before(function(done){
-				azuqua.flos(function(error, flos){
-					done();
-				});
-			});
+		  describe('Client flo invocation tests', function () {
 
-			it("Test flo should return the same starting data", function(done){
-				azuqua.invoke("httptohttp", { a: 1 }).then(function(data){
-					assert.ok(data);
-					done();
-				}, function(error){
-					assert.isNull(error);
-					done();
-				});
-			});
+      var azuqua = new Azuqua(testCredentials.accessKey, testCredentials.accessSecret);
 
-			it("Test flo should fail if the accessSecret is invalid", function(done){
-				azuqua.account.accessSecret = "1";
-				azuqua.invoke("httptohttp", { a: 1 }).then(function(data){
-					assert.isNull(data);
-					done();
-				}, function(error){
-					assert.isNotNull(error);
-					done();
-				});
-			});
-			
-		});
+      before(function (done) {
+        azuqua.flos(function (error, flos) {
+          done();
+        });
+      });
 
-	});
+      it('Test flo should return the same starting data', function (done) {
+        azuqua.invoke('httptohttp', { a: 1 }).then(function (data) {
+          assert.ok(data);
+          done();
+        }, function (error) {
+          assert.isNull(error);
+          done();
+        });
+      });
+
+      it('Test flo should fail if the accessSecret is invalid', function (done) {
+        azuqua.account.accessSecret = '1';
+        azuqua.invoke('httptohttp', { a: 1 }).then(function (data) {
+          assert.isNull(data);
+          done();
+        }, function (error) {
+          assert.isNotNull(error);
+          done();
+        });
+      });
+
+    });
+
+  });
 
 });
 
